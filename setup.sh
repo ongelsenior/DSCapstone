c78d3c0c5c363b0fd2135484ea8c7fb067eaa5a4
ongelsenior <seniorongel@yahoo.com>
2023-08-19 00:54:59 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"your-email@example.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
